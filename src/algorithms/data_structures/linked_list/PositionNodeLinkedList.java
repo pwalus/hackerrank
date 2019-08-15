fb7aac614b08e9a09e47e01ddcf45195b09dac0d
pwalus <patryk.walus@ageno.pl>
2019-08-15 19:50:29 +0200

    Algorithms for linked list

--- src/algorithms/data_structures/linked_list/PositionNodeLinkedList.java
+++ src/algorithms/data_structures/linked_list/PositionNodeLinkedList.java
@@ -73,18 +73,83 @@ public class PositionNodeLinkedList {
         int data,
         int position
     ) {
-//        if(position == 0){
-//
-//        }
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
 
-        return null;
+        if (position == 0) {
+            node.next = head;
+            return node;
+        }
+
+        SinglyLinkedListNode tmpNode = head;
+        for (int i = 1; i < position; i++) {
+            tmpNode = tmpNode.next;
+        }
+
+        node.next = tmpNode.next;
+        tmpNode.next = node;
+
+        return head;
+    }
+
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+        if (position == 0) {
+            SinglyLinkedListNode node = head.next;
+            head.next = null;
+
+            return node;
+        }
+
+        SinglyLinkedListNode tmpNode = head;
+        for (int i = 1; i < position; i++) {
+            tmpNode = tmpNode.next;
+        }
+
+        tmpNode.next = tmpNode.next != null ? tmpNode.next.next : null;
+
+        return head;
+    }
+
+    static void reversePrint(SinglyLinkedListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        reversePrint(head.next);
+        System.out.println(head.data);
+    }
+
+
+    protected static SinglyLinkedListNode reversedHead;
+
+    static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        _reverse(head, head.next);
+
+        return reversedHead;
+    }
+
+    private static SinglyLinkedListNode _reverse(
+        SinglyLinkedListNode head,
+        SinglyLinkedListNode next
+    ) {
+        if (next == null) {
+            reversedHead = new SinglyLinkedListNode(head.data);
+            return reversedHead;
+        }
+
+        SinglyLinkedListNode newHead = _reverse(next, next.next);
+        newHead.next = new SinglyLinkedListNode(head.data);
+
+        return newHead.next;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(
-            new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         SinglyLinkedList llist = new SinglyLinkedList();
 
@@ -98,13 +163,14 @@ public class PositionNodeLinkedList {
             llist.insertNode(llistItem);
         }
 
-        int data = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//        int data = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        int position = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int position = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+//        SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+        SinglyLinkedListNode llist_head = reverse(llist.head);
 
         printSinglyLinkedList(llist_head, " ", bufferedWriter);
         bufferedWriter.newLine();
